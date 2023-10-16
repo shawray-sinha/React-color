95a4b6cf1d0b7d5017119dc07fe50784b3f42a50
shawray sinha <144587302+shawray-sinha@users.noreply.github.com>
2023-10-16 15:07:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Container } from './components/Container';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div >
+
+      <Container/>
+
+    </div>
+  );
+}
+
+export default App;
